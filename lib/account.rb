84942c42aa54ef7f9eb44b11292bb75a297d59cf
keith(Toshiba - mac) <ka33145@googlemail.com>
2017-11-24 11:11:12 +0000

    Added deposit method to account

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,14 @@
 class Account
   def initialize(date = Time.now.strftime('%d/%m/%Y'), amount = 0)
-    @log = [[date, amount.to_f, 0.00, amount]]
+    @logs = [[date, amount.to_f, 0.00, amount]]
   end
 
   def get_balance
-    @log.last[3]
+    @logs.last[3]
+  end
+
+  def deposit(amount, date = Time.now.strftime('%d/%m/%Y'))
+    @logs << [date,amount.to_f,0.00,get_balance + amount]
+    "#{amount} deposit. New balance is :#{get_balance}"
   end
 end
