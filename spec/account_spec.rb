60ddd899e20fcb908a383074e0dfc79575fedf0f
keith(Toshiba - mac) <ka33145@googlemail.com>
2017-11-26 13:15:32 +0000

    Corrected typo

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,17 +2,17 @@ require 'account'
 require 'timecop'
 describe Account do
   subject(:account) { described_class.new }
-  let(:amount1) { 1000 }
-  let(:amount2) { 250 }
-  describe '#get_balance' do
+  let(:amount1) { 1000.0 }
+  let(:amount2) { 250.0 }
+  describe '#current_balance' do
     context 'No transcation' do
       it 'return startup balance' do
-        expect(account.get_balance).to eq 0.0
+        expect(account.current_balance).to eq 0.0
       end
       it 'return current balance' do
         account.deposit(amount1)
         account.withdraw(amount2)
-        expect(account.get_balance).to eq 750.0
+        expect(account.current_balance).to eq 750.0
       end
     end
   end
@@ -31,14 +31,16 @@ describe Account do
   describe '#logs' do
     context 'No transcation made' do
       it 'return account log' do
+        account = Account.new(0.0, '24/11/2017')
         result = [['24/11/2017', 0.0, 0.0, 0.0]]
         expect(account.logs).to eq result
       end
     end
     context 'transcation' do
       it 'return account log' do
-        account.deposit(amount1)
-        account.withdraw(amount2)
+        account = Account.new(0.0, '24/11/2017')
+        account.deposit(amount1, '24/11/2017')
+        account.withdraw(amount2, '24/11/2017')
         result = [
           ['24/11/2017', 0.0, 0.0, 0.0],
           ['24/11/2017', 1000.0, 0.0, 1000.0],
