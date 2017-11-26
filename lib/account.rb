3cbf98e11b098da0b74596b699e01e21f0ce7be0
keith(Toshiba - mac) <ka33145@googlemail.com>
2017-11-26 12:56:00 +0000

    Fixed typo and renamed get_balance to current_balance

--- lib/account.rb
+++ lib/account.rb
@@ -1,20 +1,20 @@
 class Account
   attr_reader :logs
-  def initialize(amount = 0 ,date = Time.now.strftime('%d/%m/%Y'))
-    @logs = [[date, amount.to_f, 0.00, amount]]
+  def initialize(amount = 0, date = Time.now.strftime('%d/%m/%Y'))
+    @logs = [[date, amount.to_f, 0.00, amount.to_f]]
   end
 
-  def get_balance
+  def current_balance
     @logs.last[3]
   end
 
   def deposit(amount, date = Time.now.strftime('%d/%m/%Y'))
-    @logs << [date, amount.to_f, 0.00, get_balance + amount]
-    "#{amount} deposited. New balance is :#{get_balance}"
+    @logs << [date, amount.to_f, 0.00, current_balance + amount]
+    "#{amount} deposited. New balance is :#{current_balance}"
   end
 
   def withdraw(amount, date = Time.now.strftime('%d/%m/%Y'))
-    @logs << [date, 0.00, amount.to_f, get_balance - amount]
-    "#{amount} withdrawn. New balance is :#{get_balance}"
+    @logs << [date, 0.00, amount.to_f, current_balance - amount]
+    "#{amount} withdrawn. New balance is :#{current_balance}"
   end
 end
